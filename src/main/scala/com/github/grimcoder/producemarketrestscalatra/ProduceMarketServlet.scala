40d247ba0ead9428fee447bb4ca98c6b2095420e
grimcoder <papochka12>
2015-09-09 00:27:13 -0700

    impelemented delete and post

--- src/main/scala/com/github/grimcoder/producemarketrestscalatra/ProduceMarketServlet.scala
+++ src/main/scala/com/github/grimcoder/producemarketrestscalatra/ProduceMarketServlet.scala
@@ -7,24 +7,36 @@ import net.liftweb.json._
 import org.scalatra._
 import org.scalatra.liftjson.LiftJsonSupport
 import org.scalatra.scalate.ScalateSupport
-
+import com.mongodb.casbah.Imports._
 
 class ProduceMarketServlet extends ScalatraServlet with ScalateSupport with LiftJsonSupport with CorsSupport {
 
-  var stream : InputStream = getClass.getResourceAsStream("/prices.json")
-  def lines  = scala.io.Source.fromInputStream( stream ).getLines.mkString
-  val prices = Serialization.read[List[Price]](lines);
+  var stream: InputStream = getClass.getResourceAsStream("/prices.json")
+
+  val lines = scala.io.Source.fromInputStream(stream).getLines.mkString
+
+  var prices = Serialization.read[List[Price]](lines);
+
+  val sstream: InputStream  = getClass.getResourceAsStream("/sales.json")
+
+  val slines = scala.io.Source.fromInputStream(sstream).getLines.mkString
+
+  var sales : List[Sale] = List[Sale]() ;
+
 
   implicit val formatsz = new DefaultFormats {
     override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   }
 
-  options("/*"){
-    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+  options("/*") {
+    response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept");
+    response.setHeader("Access-Control-Allow-Methods", "HEAD,GET,PUT,POST,DELETE,OPTIONS");
+    200
+
   }
 
   get("/api/prices") {
-    if (request.parameters.contains("id")){
+    if (request.parameters.contains("id")) {
       val id = params("id").toString
       val filtered = prices.filter(price => price.Id == Some(id))
       Extraction.decompose(
@@ -35,10 +47,14 @@ class ProduceMarketServlet extends ScalatraServlet with ScalateSupport with Lift
   }
 
   get("/api/sales") {
-  stream = getClass.getResourceAsStream("/sales.json")
-  val sales = Serialization.read[List[Sale]](lines);
 
-    if (request.parameters.contains("id")){
+    sales = sales.length match {
+      case 0 => Serialization.read[List[Sale]](slines);
+      case _ => sales;
+    }
+
+
+    if (request.parameters.contains("id")) {
       val id = params("id").toString
 
       val filtered = sales.filter(_.Id == Some(id))
@@ -46,23 +62,77 @@ class ProduceMarketServlet extends ScalatraServlet with ScalateSupport with Lift
         filtered
       )
     }
+
     else Extraction.decompose(sales)
   }
 
   post("/api/prices") {
 
-    val price : Price = parsedBody.extract[Price]
-    val a = 10
+    val price: Price = parsedBody.extract[Price]
+    price.Id match  {
+      case None => {
+
+        val maxId = prices.map(_.Id.get.toInt).max + 1
+
+        val newPrice = Price(Some(maxId.toString), price.Price, price.ItemName)
+
+        prices = newPrice :: prices
+      }
+      case Some(id) => {
+
+        prices = prices.remove(_.Id==price.Id)
+
+        prices = price :: prices
+
+      }
+    }
 
   }
 
+
+  post("/api/sales") {
+
+    val sale: Sale = parsedBody.extract[Sale]
+    sale.Id match  {
+      case None => {
+
+        val maxId = sales.map(_.Id.get.toInt).max + 1
+
+        val newPrice = Sale(Some(maxId.toString), sale.Date, sale.SaleDetails)
+
+        sales = newPrice :: sales
+      }
+      case Some(id) => {
+
+        sales = sales.remove(_.Id==sale.Id)
+
+        sales = sale :: sales
+
+      }
+    }
+
+  }
+
+
+  delete("/api/prices"){
+    val id = params("id").toString;
+    prices = prices.filter(price => price.Id != Some(id));
+  }
+
+
+  delete("/api/sales"){
+    val id = params("id").toString;
+    sales = sales.filter(price => price.Id != Some(id));
+  }
+
+
   notFound {
     // remove content type in case it was set through an action
-    contentType = null 
+    contentType = null
     // Try to render a ScalateTemplate if no route matched
     findTemplate(requestPath) map { path =>
       contentType = "text/html"
       layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound() 
+    } orElse serveStaticResource() getOrElse resourceNotFound()
   }
 }
